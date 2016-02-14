e0b88e00a535a7720efda9e232b0ef6d97d51a9c
ilikecola <knight.lost.heaven@gmail.com>
2016-02-15 03:32:34 +0800

    First Update

---
+++ WindowsFormsApplication1/ScreenCapture.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class ScreenCapture
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.manual = new System.Windows.Forms.Timer(this.components);
+            this.button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // manual
+            // 
+            this.manual.Tick += new System.EventHandler(this.manual_Tick);
+            // 
+            // button
+            // 
+            this.button.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button.Location = new System.Drawing.Point(12, 12);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(78, 25);
+            this.button.TabIndex = 140;
+            this.button.Text = "手动获取";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button_MouseDown);
+            this.button.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(107, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 141;
+            this.label1.Text = "000000";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F);
+            this.button1.Location = new System.Drawing.Point(12, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 25);
+            this.button1.TabIndex = 142;
+            this.button1.Text = "截图";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 143;
+            this.label2.Text = "000000";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F);
+            this.button2.Location = new System.Drawing.Point(12, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 25);
+            this.button2.TabIndex = 144;
+            this.button2.Text = " 取色";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 145;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(92, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
+            this.textBox2.TabIndex = 146;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 13);
+            this.label3.TabIndex = 147;
+            this.label3.Text = "x";
+            // 
+            // ScreenCapture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 145);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button);
+            this.Name = "ScreenCapture";
+            this.Text = "ScreenCapture";
+            this.Load += new System.EventHandler(this.ScreenCapture_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer manual;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication1/ScreenCapture.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class ScreenCapture : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "WindowFromPoint")]
+        public static extern IntPtr WindowFromPoint(Point point);
+        [DllImport("gdi32.dll", EntryPoint = "GetPixel")]//取指定点颜色
+        private static extern int GetPixel(IntPtr hdc, Point p);
+        private class GDI32
+        {
+            public const int SRCCOPY = 0x00CC0020;
+            [DllImport("gdi32.dll")]
+            public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest,
+                int nWidth, int nHeight, IntPtr hObjectSource,
+                int nXSrc, int nYSrc, int dwRop);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth,
+                int nHeight);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteObject(IntPtr hObject);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+        }
+        /// <summary>
+        /// 辅助类 定义User32 API函数
+        /// </summary>
+        private class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct RECT
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetDesktopWindow();
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowDC(IntPtr hWnd);
+            [DllImport("user32.dll")]
+            public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
+        }
+
+
+        IntPtr GamehWnd = new IntPtr(0);
+        DateTime currentTime = new DateTime();
+
+        public ScreenCapture()
+        {
+            InitializeComponent();
+        }
+
+        private void button_MouseDown(object sender, MouseEventArgs e)
+        {
+            manual.Enabled = true;
+        }
+
+        private void button_MouseUp(object sender, MouseEventArgs e)
+        {
+            manual.Enabled = false;
+        }
+
+        private void manual_Tick(object sender, EventArgs e)
+        {
+            int x = Cursor.Position.X;
+            int y = Cursor.Position.Y;
+            Point p = new Point(x, y);
+            GamehWnd = WindowFromPoint(p);
+            label1.Text = GamehWnd.ToString();
+        }
+
+        private void ScreenCapture_Load(object sender, EventArgs e)
+        {
+            manual.Enabled = false;
+            manual.Interval = 500;
+        }
+
+        public Image CaptureWindow(IntPtr handle)
+        {
+            IntPtr hdcSrc = User32.GetWindowDC(handle);
+            User32.RECT windowRect = new User32.RECT();
+            User32.GetWindowRect(handle, ref windowRect);
+            int width = windowRect.right - windowRect.left;
+            int height = windowRect.bottom - windowRect.top;
+            IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc);
+            IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc, width, height);
+            IntPtr hOld = GDI32.SelectObject(hdcDest, hBitmap);
+            GDI32.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 0, 0, GDI32.SRCCOPY);
+            GDI32.SelectObject(hdcDest, hOld);
+            GDI32.DeleteDC(hdcDest);
+            User32.ReleaseDC(handle, hdcSrc);
+            Image img = Image.FromHbitmap(hBitmap);
+            GDI32.DeleteObject(hBitmap);
+            return img;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Image img = CaptureWindow(GamehWnd);
+            string dir = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            String Timestr = currentTime.ToString("HHmmss");
+            img.Save(dir + Timestr + ".jpg", ImageFormat.Jpeg);
+            Process.Start(dir + Timestr + ".jpg");
+        }
+
+        //后台取色部分
+        public string GetPixelColor(IntPtr hWnd, int xPos, int yPos)
+        {
+            string PixelColor = "";
+            Point p = new Point(xPos, yPos + 6);
+            IntPtr hdc = User32.GetWindowDC(hWnd);
+            int c = GetPixel(hdc, p);
+            int r = (c & 0xFF);//转换R
+            int g = (c & 0xFF00) / 256;//转换G
+            int b = (c & 0xFF0000) / 65536;//转换B
+            PixelColor = b.ToString("X").PadLeft(2, '0') + g.ToString("X").PadLeft(2, '0') + r.ToString("X").PadLeft(2, '0');//输出16进制颜色
+            return PixelColor;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string pixelcolor = GetPixelColor(GamehWnd, int.Parse(textBox1.Text), int.Parse(textBox2.Text));
+            label2.Text = pixelcolor;
+        }
+    }
+}
